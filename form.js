8fd37133981c5c21249f9d5cd6029866ce2df427
izabella dobielewska <izadobielewska@yahoo.co.uk>
2023-07-10 09:08:40 +0200

    add letter counter in the add question

--- form.js
+++ form.js
@@ -1,16 +1,21 @@
-const questionElement = document.querySelector('data-js="question-field"');
-const answerElement = document.querySelector('data-js="answer-field"');
-
+const questionElement = document.querySelector('[data-js="question-field"]');
+const answerElement = document.querySelector('[data-js="answer-field"]');
+const tagElement = document.querySelector('[data-js="tag-field"]');
 const questionLettersLeft = document.querySelector(
-  'data-js="questionLettersLeft"'
+  '[data-js="questionLettersLeft"]'
 );
-const answerLettersLeft = document.querySelector('data-js="answerLettersLeft"');
+const answerLettersLeft = document.querySelector(
+  '[data-js="answerLettersLeft"]'
+);
 
-function addCharactersLeft(element, count) {
-  const maxLength = element.getAttribute("maxLength");
-  count.innerText = maxLength;
-  element.addEventListener("input", () => {
-    count.innerText = maxLength - element.value.length;
+function addCharactersLeft(inputElement, countElement) {
+  const maxLength = inputElement.maxLength;
+  countElement.innerText = "You have " + maxLength + " characters left.";
+  inputElement.addEventListener("input", () => {
+    countElement.innerText =
+      "You have " +
+      (maxLength - inputElement.value.length) +
+      " characters left.";
   });
 }
 addCharactersLeft(questionElement, questionLettersLeft);
